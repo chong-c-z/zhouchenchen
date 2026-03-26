be9a587f574234affe5d18c678de85770087a410
chong-c-z <18815777709@163.com>
2026-03-26 10:36:43 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/zhouchenchen/', // 关键：必须与你的仓库名一致
-  // ... 其他配置
+  plugins: [react()],
+  base: '/zhouchenchen/',
 })
